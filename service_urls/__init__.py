3d8a23f20c0bc6fea89503264d43ec31f32fbd13
Raffaele Salmaso <raffaele@salmaso.org>
2019-01-19 23:30:20 +0100

    Uniform to single quotes

--- service_urls/__init__.py
+++ service_urls/__init__.py
@@ -2,8 +2,8 @@ from .base import Service  # noqa: F401
 from .services import cache, db, email  # noqa: F401
 from .version import get_version
 
-VERSION = (1, 1, 0, "final", 0)
+VERSION = (1, 1, 0, 'final', 0)
 
 __version__ = get_version(VERSION)
-__author__ = "Raffaele Salmaso"
-__email__ = "raffele@salmaso.org"
+__author__ = 'Raffaele Salmaso'
+__email__ = 'raffele@salmaso.org'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import io
 import service_urls
 from setuptools import find_packages, setup
 
-with io.open('README.md', "rt", encoding='utf-8') as fp:
+with io.open('README.md', 'rt', encoding='utf-8') as fp:
     long_description = fp.read()
 
 
@@ -13,7 +13,7 @@ setup(
     version=service_urls.__version__,
     description='setting helper for django to represent databases, caches and email settings via a single string',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     author=service_urls.__author__,
     author_email=service_urls.__email__,
     url='https://bitbucket.org/rsalmaso/django-service-urls/',
@@ -23,20 +23,20 @@ setup(
     python_requires='>=3.5',
     packages=find_packages(exclude=['tests', 'tests.*']),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.1',
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )
